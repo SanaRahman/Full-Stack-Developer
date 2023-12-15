3e38c726211e2e50c6c22ca7df1984da87021b5b
SanaRahman <sanarahman930@gmail.com>
2023-12-15 17:02:39 +0500

    interview-task

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <div className="container">
+        <p className="date">March 17, 2021 12:00</p>
+        <p className="title2">The help of Augmented Reality and Holograms.</p>
+        <p className="txt">Use Augmented Reality to keep students engaged and provide detailed explanations of models and course material. In a 3D, AR Lab, students can pinch, zoom, and rotate equipment related to the course, 
+          for a fully immersive learning experience</p>
+        </div>
+        <div className="sidebar">
+          <div className="form">
+          <form>
+            <label className="label">First Name</label>
+            <input type="text" name="firstname" placeholder="name" />
+            <label className="label">Last Name</label>
+            <input type="text" name="firstname" placeholder="name" />
+            <div className="info">
+              <div>
+              <input type="checkbox" name="firstname" placeholder="name" />
+            <label className="label">Remeber Me</label>
+              </div>
+              <div>
+              <a className="link">Forgot Password</a>
+              </div>
+            
+           
+            </div>
+
+
+          </form>
+          </div>
+         
+        </div>
+      
     </div>
   );
 }
